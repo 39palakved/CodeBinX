46029d6a97c72cf6b86793d6e25b5664a291dc95
palak porwal <porwalpalak10@gmail.com>
2025-02-07 20:06:36 +0530

    first-commit

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ main().catch(err => console.error('MongoDB Connection Error:', err));
 async function main() {
   await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
   console.log('✅ DB Connected');
+  console.log('Hapieee');
 }
 
 // ✅ Define Schema & Model
